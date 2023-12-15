3fc3fc754b27ce7af1f3f0f5e4d6e4991de05203
shwetapurushe <shwetapurushe@gmail.com>
2023-12-15 16:52:58 +0530

    hf transformers -pipelines

---
+++ models/hf_tasks.py
@@ -0,0 +1,15 @@
+from transformers import pipeline
+
+# the downloaded cached model is available at ~./cache/huggingface/hub
+# Use the CLI to manage cache using the TUI at https://huggingface.co/docs/huggingface_hub/main/en/guides/cli
+
+# Text classfication
+text = "I would go to this restaurant even if you woke me from my sleep."
+# specify only task, default model selected
+# pipe1 = pipeline("text-classification") 
+
+# specify which model to use, which is then downloaded and cached
+# sentiment in terms of many different emotions, sadness, anger, inspiration, approval, etc
+pipe1 = pipeline("text-classification", model="SamLowe/roberta-base-go_emotions", top_k=5)  # top 5 emotions
+print(pipe1(text))
+
